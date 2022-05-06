85103db45ff2b4ead8a9a725c702b15f59cf327d
JessDet <89451502+JessDet@users.noreply.github.com>
2022-05-06 11:20:24 +0200

    Update recuplist.php
    ajout idDinerbar dans requete

--- recuplist.php
+++ recuplist.php
@@ -6,7 +6,7 @@ header("Access-Control-Allow-Headers: Content-Type");
 $data = json_decode(file_get_contents('php://input'), true);
 require 'pdo.php';
 
-$stateRead = $pdo->prepare('SELECT name, cp, ville, date FROM dinerBar');
+$stateRead = $pdo->prepare('SELECT idDinbar, name, cp, ville, date FROM dinerBar');
 $stateRead->execute();
 $result = $stateRead-> fetchAll();
 
